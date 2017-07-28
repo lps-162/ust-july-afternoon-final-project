fa46c17ff1b07d7c14d49a7e92bf13b2dc7e6525
LP Shivan <lp.shivan@gmail.com>
2017-07-28 15:12:10 +0530

    creating new components, home, about and navbar

--- limetools/src/app/app.module.ts
+++ limetools/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './erp/home/home.component';
+import { AboutComponent } from './erp/about/about.component';
+import { EmployeesListComponent } from './erp/employees/employees-list/employees-list.component';
+import { NavbarComponent } from './erp/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    EmployeesListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
